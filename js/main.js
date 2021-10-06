8e366ed1e3f759f6ae3b649b91dc24b4bbe689e0
Vatskovskaya <86354815+Vatskovskaya@users.noreply.github.com>
2021-10-06 22:35:48 +0300

    responsive layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.header__navigation-list').toggleClass('active');
+  })
     $('.photo-slide').slick({
     arrows: true,
     slidesToShow: 1,
